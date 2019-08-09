68eb93fd9935d2aa4ff828dc20ed24194ce4d09b
赵家豪 <reallyloveyouto@qq.com>
2019-08-09 16:33:24 +0800

    add recommend

---
+++ project/src/pages/home/components/Recommend.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <div class="title">热销推荐</div>
+        <ul>
+            <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+                <img class="item-img" :src="item.imgUrl"/>
+                <div class="item-info">
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}&nbsp;&nbsp;{{item.assess}}</p>
+                    <button class="item-button">查看详情</button>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'HomeRecommend',
+    data(){
+        return{
+            recommendList:[{
+                id:'0001',
+                imgUrl:'http://img1.qunarzz.com/sight/p0/1906/f7/f74f725a5da59bcea3.img.jpg_200x200_23304e5f.jpg',
+                title:'红花湖水上乐园',
+                desc:'⭐⭐⭐⭐⭐',
+                assess:'45条评价'
+            },{
+                id:'0002',
+                imgUrl:'http://img1.qunarzz.com/sight/p0/1603/b1/b19c46c888b4b6e790.water.jpg_200x200_522561eb.jpg',
+                title:'双月湾',
+                desc:'⭐⭐⭐⭐⭐',
+                assess:'1245条评价'
+            },{
+                id:'0003',
+                imgUrl:'http://img1.qunarzz.com/sight/p0/1711/3a/3a5b9ca51e954a76a3.img.jpg_110x110_c9a72e59.jpg',
+                title:'白盆湖温泉度假村',
+                desc:'⭐⭐⭐⭐⭐',
+                assess:'44条评价'
+            }]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+    .title
+        margin-top:.2rem
+        line-height:.8rem
+        background:#eee
+        text-indent:.2rem
+    .item
+        overflow:hidden
+        display:flex
+        height:1.9rem
+        .item-img
+            width:1.7rem
+            height:1.7rem
+            padding 0.1rem
+        .item-info
+            flex:1
+            padding:.1rem
+            min-width:0
+            .item-title
+                line-height:.54rem
+                font-size:.32rem
+                ellipsis()
+            .item-desc
+                margin-top:.05rem
+                line-height:.6rem
+                color:#ccc
+                ellipsis()
+            .item-button
+                background:#ff8c12
+                margin-top:.1rem
+                padding:0 .1rem
+                border-radius: .06rem
+</style>
---
+++ project/src/pages/home/components/Weekend.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <div class="title">周末去哪儿</div>
+        <ul>
+            <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+                <div class="item-img-wrapper">
+                    <img class="item-img" :src="item.imgUrl"/>
+                </div>
+                <div class="item-info">
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}&nbsp;&nbsp;{{item.assess}}</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'HomeWeekend',
+    data(){
+        return{
+            recommendList:[{
+                id:'0001',
+                imgUrl:'http://img1.qunarzz.com/sight/source/1507/d9/2ddc7652fdba16.jpg_r_640x214_5e62f1a3.jpg',
+                title:'深圳周边游',
+                desc:'远离城市拥堵，去周边开启清新假期吧。'
+            },{
+                id:'0002',
+                imgUrl:'http://img1.qunarzz.com/sight/source/1505/4a/ac007012d54bd4.jpg_r_640x214_eefa6ade.jpg',
+                title:'惠州打卡圣地',
+                desc:'毓秀山水，继续着四季轮回的交替'
+            },{
+                id:'0003',
+                imgUrl:'http://img1.qunarzz.com/sight/source/1812/40/678d1e6eb29766.jpg_r_640x214_750d6d68.jpg',
+                title:'惠州亲子长隆',
+                desc:'世界欢乐汇聚长隆，这里拥有多个大型游乐设备和精彩的演出，和小伙伴们一起感受刺激的体验'
+            }]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+    .title
+        margin-top:.2rem
+        line-height:.8rem
+        background:#eee
+        text-indent:.2rem
+    .item-img-wrapper
+        overflow:hidden
+        height:0
+        padding-bottom:37.8%
+    .item-img
+        width:100%
+    .item-info
+        padding:.1rem
+        .item-title
+            line-height:.48rem
+            font-size:.32rem
+            ellipsis()
+        .item-desc
+            margin-top:.02rem
+            line-height:.6rem
+            color:#ccc
+            ellipsis()
+</style>
