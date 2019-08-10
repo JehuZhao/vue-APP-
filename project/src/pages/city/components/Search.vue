67b6dafb03f405fd5061c85e319c30c3277fd353
赵家豪 <reallyloveyouto@qq.com>
2019-08-10 19:31:18 +0800

    finish city css

--- project/src/pages/city/components/Search.vue
+++ project/src/pages/city/components/Search.vue
@@ -13,7 +13,7 @@ export default {
 <style lang="stylus" scoped>
     @import '~styles/varibles.styl'
     .search
-        height:.72rem
+        height:.64rem
         padding:0.1rem
         background:$bgColor
         .search-input
@@ -22,6 +22,6 @@ export default {
             height:.62rem
             padding: 0 0.1rem
             text-align:center
-            border-radius:.06rem
+            border-radius:.2rem
             color:#666
 </style>
